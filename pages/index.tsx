324605a3b22d1510134f1d120a66337c1bfa4284
harenohee <panda4859@gmail.com>
2023-09-01 16:11:22 +0900

    refactor: index 모듈화 적용
    - getAllPosts
    
    #4

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,6 @@
-import fs from "fs";
-import { join } from "path";
-import matter from "gray-matter";
 import PostType from "../interfaces/post";
 import List from "../components/List";
-
+import { getAllPosts } from "../lib/api";
 type Props = {
   allPosts: PostType[];
 };
@@ -18,15 +15,7 @@ export default function Blog({ allPosts }: Props) {
 }
 
 export async function getStaticProps() {
-  // __post 폴더에서 파일 읽어옴
-  const files = fs.readdirSync(join(process.cwd(), "__post"));
-  const allPosts = files
-    .map((file) => {
-      const rawData = fs.readFileSync(join("__post", file), "utf-8");
-      const { data } = matter(rawData);
-      return data;
-    })
-    .sort((a, b) => (a.date > b.date ? -1 : 1));
+  const allPosts = getAllPosts();
 
   return {
     props: {
