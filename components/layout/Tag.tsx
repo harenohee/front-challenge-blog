58df0dbc177f5c50013b389008fea64e40d7063e
harenohee <panda4859@gmail.com>
2023-09-25 21:32:00 +0900

    feat: Tag for dynamic route
    - dynamic route를 위한 Link태그 추가
    
    #12

--- components/layout/Tag.tsx
+++ components/layout/Tag.tsx
@@ -1,6 +1,6 @@
 import { useRecoilState } from "recoil";
-import { tagState } from "../../recoil/TagState";
-import { MouseEvent } from "react";
+import { tagState } from "../../recoil/tagState";
+import Link from "next/link";
 interface ITags {
   tag: string;
 }
@@ -8,19 +8,26 @@ const Tag = ({ tag }: ITags) => {
   const [clickedTag, setClickedTag] = useRecoilState(tagState);
   // 태그 클릭 -> 상태 변경
   const handleClickedTagName = (event: EventTarget & HTMLButtonElement) => {
-    console.log(event.value);
+    setClickedTag(() => event.value);
+    // 딜레이있음 -> useEffect ? useCallback?
   };
   return (
     <div className="w-10">
-      <button
-        className="font-semibold rounded-md"
-        onClick={(e) => handleClickedTagName(e.currentTarget)}
-        value={tag}
+      <Link
+        href={{
+          pathname: "/[tag]",
+          query: { tag: tag },
+        }}
       >
-        {tag}
-      </button>
+        <button
+          className="font-semibold rounded-md"
+          onClick={(e) => handleClickedTagName(e.currentTarget)}
+          value={tag}
+        >
+          {tag}
+        </button>
+      </Link>
     </div>
   );
 };
-
 export default Tag;
