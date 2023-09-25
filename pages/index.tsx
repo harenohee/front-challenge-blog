75ee01e72b590144a2a4a87abbb3d18814bf2130
harenohee <panda4859@gmail.com>
2023-09-25 21:33:28 +0900

    feat: Tag 컴포넌트 추가
    - 태그 이름(임시)
    
    #12

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,31 @@
-import PostType from "../interfaces/post";
+import { PostType } from "../interfaces/post";
 import List from "../components/List";
 import { getAllPosts } from "../lib/api";
+import Tag from "../components/layout/Tag";
+
 type Props = {
   allPosts: PostType[];
 };
 export default function Blog({ allPosts }: Props) {
   return (
-    <ul>
-      {allPosts.map((post) => (
-        <List post={post} key={post.slug} />
-      ))}
-    </ul>
+    <div>
+      <div className="flex justify-between">
+        <Tag tag="Job" />
+        <Tag tag="CSS" />
+        <Tag tag="Career" />
+        <Tag tag="React" />
+      </div>
+      <ul>
+        {allPosts.map((post) => (
+          <List post={post} key={post.slug} />
+        ))}
+      </ul>
+    </div>
   );
 }
 
 export async function getStaticProps() {
   const allPosts = getAllPosts();
-
   return {
     props: {
       allPosts,
